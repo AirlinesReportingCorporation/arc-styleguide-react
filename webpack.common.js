31008f65f1896a5d4fe483140860d9e26fd52807
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-03-08 14:46:53 -0500

    boilerplate

---
+++ webpack.common.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+
+const extractSass = new MiniCssExtractPlugin({
+  filename: "[name].min.css",
+  chunkFilename: "[id].css"
+});
+
+module.exports = {
+  entry: {
+    custom: "./src/index.jsx",
+  },
+  output: {
+    filename: "[name].min.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader?url=false",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.js"
+    },
+    extensions: ["*", ".js", ".jsx"]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    extractSass
+  ]
+};
