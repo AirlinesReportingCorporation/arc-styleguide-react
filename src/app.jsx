644b58bac3b4f9a3a661ff9c64b4681838e5281b
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-04-12 16:17:12 -0400

    add top nav menu and style top and side navs

--- src/app.jsx
+++ src/app.jsx
@@ -33,59 +33,64 @@ class App extends Component {
       <div style={{ width: "100%" }}>
         <div className="top_nav">
             <div className="icon-arc-logo"></div>
+            <div className="styles-top-menu">
+              <ul>Brand Overview</ul>
+              <ul>Style Guide</ul>
+              <ul>Media Kit</ul>
+            </div>
           </div>
         <div class="flex_container">
           <div class="flex_side_sticky">
              <div className="navbar">
                <div className="nav-menu">
-                 <div className="nav-menu-items style-main-item">
+                 <ul className="nav-menu-items style-main-item">
                    Our Brand
-                 </div>
-                 <div className="nav-menu-items style-main-item">
+                 </ul>
+                 <ul className="nav-menu-items style-main-item">
                    Using Our Brand
-                 </div>
-                 <div className="nav-menu-items style-sub-item">
+                 </ul>
+                 <ul className="nav-menu-items style-sub-item">
                    Brand Language
-                 </div>
-                 <div className="nav-menu-items style-sub-item">
+                 </ul>
+                 <ul className="nav-menu-items style-sub-item">
                    Brand Voice
-                 </div>
-                 <div className="nav-menu-items style-main-item">
+                 </ul>
+                 <ul className="nav-menu-items style-main-item">
                   Visual Identity
-                 </div>
-                 <div className="nav-menu-items style-sub-item">
+                 </ul>
+                 <ul className="nav-menu-items style-sub-item">
                    Logo
-                 </div>
-                 <div className="nav-menu-items style-sub-item">
+                 </ul>
+                 <ul className="nav-menu-items style-sub-item">
                    Typography
-                 </div>
-                 <div className="nav-menu-items style-sub-item">
+                 </ul>
+                 <ul className="nav-menu-items style-sub-item">
                    Color Palette
-                 </div>
-                 <div className="nav-menu-items style-main-item">
+                 </ul>
+                 <ul className="nav-menu-items style-main-item">
                    Writing Style Guide & Best Practices
-                 </div>
-                 <div className="nav-menu-items style-sub-item">
+                 </ul>
+                 <ul className="nav-menu-items style-sub-item">
                    Frequently Used Terms
-                 </div>
-                 <div className="nav-menu-items style-sub-item">
+                 </ul>
+                 <ul className="nav-menu-items style-sub-item">
                    Formatting Dates, Times, & Numbers
-                 </div>
-                 <div className="nav-menu-items style-sub-item">
+                 </ul>
+                 <ul className="nav-menu-items style-sub-item">
                    Grammar & Punctuating
-                 </div>
-                 <div className="nav-menu-items style-main-item">
+                 </ul>
+                 <ul className="nav-menu-items style-main-item">
                    Web Style Guide
-                 </div>
-                 <div className="nav-menu-items style-sub-item">
+                 </ul>
+                 <ul className="nav-menu-items style-sub-item">
                    Typography
-                 </div>
-                 <div className="nav-menu-items style-sub-item">
+                 </ul>
+                 <ul className="nav-menu-items style-sub-item">
                    Components
-                 </div>
-                 <div className="nav-menu-items style-sub-item">
+                 </ul>
+                 <ul className="nav-menu-items style-sub-item">
                    Best Practices
-                 </div>
+                 </ul>
                </div>
              </div>
           </div>
