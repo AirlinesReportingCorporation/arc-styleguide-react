1aeab84be5fcb268fbc191477d86c92677f24932
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-04-15 17:17:27 -0400

    configure the bottom area of the term usage

--- src/components/termusage.jsx
+++ src/components/termusage.jsx
@@ -18,14 +18,20 @@ class TermsUsage extends Component {
               <div className="term-usage-copy">{this.props.incorrectCopy}</div>
             </div>
             <div className="incorrect-bar"></div>
-            <p className="incorrect-explaination">{this.props.incorrectInfo}</p>
+            <div className="bottom-area">
+            <h5 className="term-type">Incorrect</h5>
+            <p className="term-explaination">{this.props.incorrectInfo}</p>
+            </div>
           </div>
           <div className="col-lg-6">
             <div className="correct">
               <div className="term-usage-copy">{this.props.correctCopy}</div>
             </div>
             <div className="correct-bar"></div>
-            <p className="correct-explaination">{this.props.correctInfo}</p>
+            <div className="bottom-area">
+            <h5 className="term-type">Correct</h5>
+            <p className="term-explaination">{this.props.correctInfo}</p>
+            </div>
           </div>
         </div>
       </div>
