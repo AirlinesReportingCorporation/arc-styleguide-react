1e204c16f40d841c6f9f37ccdc7d449ebb404981
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-04-26 13:53:02 -0400

    add in copy for visual style

--- src/visual-identity.jsx
+++ src/visual-identity.jsx
@@ -50,70 +50,262 @@ class Visual extends Component {
                 </p>
               </div>
             </div>
-            <TermDef />
-            <TermDef />
-            <div className="row">
-              <div className="col-lg-6">
-                {/* <Box /> */}
-              </div>
-              <div className="col-lg-6">
-                {/* <Box /> */}
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-lg-3">
-                {/* <Box /> */}
-              </div>
-              <div className="col-lg-3">
-                {/* <Box /> */}
-              </div>
-              <div className="col-lg-3">
-                {/* <Box /> */}
-              </div>
-              <div className="col-lg-3">
-                {/* <Box /> */}
-              </div>
-            </div>
-            <div className="row">
-                <h2></h2>
-                <div></div>
-                <button className="ctaBtn">Download</button>
-            </div>
-            <div className="row">
-                <h2></h2>
-                <p></p>
-                <div className="typography-block">Typogaphy box here</div>
-            </div>
-            <div className="row">
-                <h3></h3>
+            <TermDef
+              term="Primary Logo"
+              definition={
                 <p>
+                  The primary ARC logo appears in our brand teal and is the
+                  preferred method of communication wherever possible. It is
+                  important that the basic elements of our brand are represented
+                  consistently across all applications. Logo artwork must never
+                  be modified in scale or color outside of specific uses
+                  outlined here.
                 </p>
-            </div>
-            <TermsUsage incorrectCopy="" incorrectInfo="" correctCopy="" correctInfo=""/>
-            <TermsUsage incorrectCopy="" incorrectInfo="" correctCopy="" correctInfo=""/>
-            <TermsUsage incorrectCopy="" incorrectInfo="" correctCopy="" correctInfo=""/>
-            <TermsUsage incorrectCopy="" incorrectInfo="" correctCopy="" correctInfo=""/>
-            <TermsUsage incorrectCopy="" incorrectInfo="" correctCopy="" correctInfo=""/>
-            <TermsUsage incorrectCopy="" incorrectInfo="" correctCopy="" correctInfo=""/>
+              }
+            />
+            <TermDef
+              term="Secondary Logo With Full Company Name"
+              definition={
+                <div>
+                  <p>
+                    For secondary logo applications, the ARC logo with the full
+                    company name can be used. As with our primary logo, this
+                    logo can be used in regular and knock-out applications, in
+                    colors from our primary brand palette of navy and teal, as
+                    well as grayscale.
+                  </p>
+                  <p>
+                    Note that the logo with full company name should not be used
+                    under two inches in width. For cases under two inches, use
+                    the logo without the full company name.
+                  </p>
+                </div>
+              }
+            />
             <div className="row">
-                <h2></h2>
-                <p></p>
+              <div className="col-lg-6">{/* <Box /> */}</div>
+              <div className="col-lg-6">{/* <Box /> */}</div>
+            </div>
+            <div className="row">
+              <div className="col-lg-3">{/* <Box /> */}</div>
+              <div className="col-lg-3">{/* <Box /> */}</div>
+              <div className="col-lg-3">{/* <Box /> */}</div>
+              <div className="col-lg-3">{/* <Box /> */}</div>
+            </div>
+            <h2>Logo Suite</h2>
+            <div>
+              Download our logo suite containing different sizes, colors and
+              file types.
+            </div>
+            <button className="ctaBtn">Download</button>
+            <div className="row">
+              <h2>Typography</h2>
+              <p>
+                Just as important as iconography and use of color, is the use of
+                unique typography to attach a deeper meaning to your writing and
+                accentuate our storytelling. Our marketing materials pair the
+                modern and clean design of Source Sans with the friendly Roboto
+                Slab, a combination that reflects ARC’s authority and
+                approachability. The standardized fonts Arial Black and Palatino
+                provide easy-to-use alternatives for our general tools and
+                day-to-day communications.
+              </p>
+              <div className="typography-block">Typogaphy box here</div>
+            </div>
+            <div className="row">
+              <h3>Typography Usage</h3>
+              <p>
+                Our brand palette serves to underscore our brand history and
+                unique voice. Grounded in strategy and insights, our palette
+                communicates a sense of authority, intelligence, legacy,
+                decisiveness and confidence.
+              </p>
+            </div>
+            <TermsUsage
+              incorrectCopy={
+                <div>
+                  <h5>I am a headline</h5>
+                  <p style={{ fontWeight: "bold" }}>
+                    I am not a headline but I use the same weight as a headline.
+                    I am not a headline but I use the same weight as a headline.
+                    I am not a headline but I use the same weight as a headline.
+                    I am not a headline but I use the same weight as a headline.{" "}
+                  </p>
+                </div>
+              }
+              incorrectInfo="Do not use the same weight for different levels of text hierarchy."
+              correctCopy={
+                <div>
+                  <h5>I am a headline</h5>
+                  <p>
+                    I am not a headline, I am body text, and I use a different
+                    weight to a headline. I am not a headline, I am body text,
+                    and I use a different weight to a headline. I am not a
+                    headline, I am body text, and I use a different weight to a
+                    headline.
+                  </p>
+                </div>
+              }
+              correctInfo="Use differentiating weights for different sections of text heirarchy."
+            />
+            <TermsUsage
+              incorrectCopy={
+                <div>
+                  <h5>I am a headline</h5>
+                  <h6>I am a sub-head</h6>
+                  <p>
+                    I am body copy but I am the same size as the headline and
+                    the sub-head{" "}
+                  </p>
+                </div>
+              }
+              incorrectInfo="Do not use the same point size for different levels of text hierarchy."
+              correctCopy={
+                <div>
+                  <h5>I am a headline</h5>
+                  <h6>I am a sub-head</h6>
+                  <p>
+                    I am body copy but I am the same size as the headline and
+                    the sub-head
+                  </p>
+                </div>
+              }
+              correctInfo="Use differentiating weights for different sections of content."
+            />
+            <TermsUsage
+              incorrectCopy={
+                <div>
+                  <h5>I am a headline</h5>
+                  <h6>I am a sub-head</h6>
+                  <p>
+                    I am body copy but I am the same size as the headline and
+                    the sub-head
+                  </p>
+                </div>
+              }
+              incorrectInfo="Do not misalign your text."
+              correctCopy={
+                <div>
+                  <h5>I am a headline</h5>
+                  <h6>I am a sub-head</h6>
+                  <p>
+                    I am body copy but I am the same size as the headline and
+                    the sub-head
+                  </p>
+                </div>
+              }
+              correctInfo="Align your text."
+            />
+            <TermsUsage
+              incorrectCopy={
+                <div>
+                  <h5>Lorem ip s um</h5>
+                  <div>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                    sed aliquam turpis, ac condimentum libero. Donec consectetur
+                    congue felis quis volutpat. Etiam et pulvinar nisi.
+                  </div>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                    sed aliquam turpis, ac condimentum libero. Donec consectetur
+                    congue felis quis volutpat. Etiam et pulvinar nisi.
+                  </p>
+                </div>
+              }
+              incorrectInfo="Do not adjust the tracking or kerning of text."
+              correctCopy={
+                <div>
+                  <h5>Lorem ip s um</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                    sed aliquam turpis, ac condimentum libero. Donec consectetur
+                    congue felis quis volutpat. Etiam et pulvinar nisi.
+                  </p>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                    sed aliquam turpis, ac condimentum libero. Donec consectetur
+                    congue felis quis volutpat. Etiam et pulvinar nisi.
+                  </p>
+                </div>
+              }
+              correctInfo="A good rule of thumb is to keep the font’s natural tracking and kerning."
+            />
+            <TermsUsage
+              incorrectCopy={
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  sed aliquam turpis, ac condimentum libero. Donec consectetur
+                  congue felis quis volutpat. Etiam et pulvinar nisi. Vivamus
+                  sollicitudin lectus quis tortor vehicula, at pretium massa
+                  imperdiet.
+                </p>
+              }
+              incorrectInfo="Do not overuse emphasis."
+              correctCopy={
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  sed aliquam turpis, ac condimentum libero. Donec consectetur
+                  congue felis quis volutpat. Etiam et pulvinar nisi. Vivamus
+                  sollicitudin lectus quis tortor vehicula, at pretium massa
+                  imperdiet.
+                </p>
+              }
+              correctInfo="Keep copy consistent."
+            />
+            <TermsUsage
+              incorrectCopy={
+                <div>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                    sed aliquam turpis, ac condimentum libero. Donec consectetur
+                    congue felis quis volutpat. Etiam et pulvinar nisi. Vivamus
+                    sollicitudin lectus quis tortor.
+                  </p>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </div>
+              }
+              incorrectInfo=""
+              correctCopy={
+                <div>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                    sed aliquam turpis, ac condimentum libero. Donec consectetur
+                    congue felis quis volutpat. Etiam et pulvinar nisi. Vivamus
+                    sollicitudin lectus quis tortor.
+                  </p>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                    sed aliquam turpis, ac condimentum libero. Donec consectetur
+                    congue felis quis volutpat. Etiam et pulvinar nisi. Vivamus
+                    sollicitudin lectus quis tortor.
+                  </p>
+                </div>
+              }
+              correctInfo=""
+            />
+            <div className="row">
+              <h2></h2>
+              <p></p>
             </div>
             <h3></h3>
             <div className="row">
-                <ColorBox name="ARC Teal" pms="229C (7421U)" cmyk="55, 98, 47, 54" rgb="07, 28, 58" hex="#189BB0"/>
-                <ColorBox pms="" cmyk="" rgb="" hex=""/>
-                <ColorBox pms="" cmyk="" rgb="" hex=""/>
-                <ColorBox pms="" cmyk="" rgb="" hex=""/>
+              <ColorBox
+                name="ARC Teal"
+                pms="229C (7421U)"
+                cmyk="55, 98, 47, 54"
+                rgb="07, 28, 58"
+                hex="#189BB0"
+              />
+              <ColorBox pms="" cmyk="" rgb="" hex="" />
+              <ColorBox pms="" cmyk="" rgb="" hex="" />
+              <ColorBox pms="" cmyk="" rgb="" hex="" />
             </div>
             <h3></h3>
-            <div className="row">
-                {/* <AccordianColor /> */}
-            </div>
+            <div className="row">{/* <AccordianColor /> */}</div>
             <h3></h3>
-            <div className="row">
-                {/* <AccordianColor /> */}
-            </div>
+            <div className="row">{/* <AccordianColor /> */}</div>
             <h3></h3>
             <p></p>
           </div>
