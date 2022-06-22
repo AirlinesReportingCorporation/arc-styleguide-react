a684dea3933a09906f19d0aae375bf1a986c75a6
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-06-22 12:38:59 -0400

    adjust top nav

--- src/components/topnav.jsx
+++ src/components/topnav.jsx
@@ -11,13 +11,30 @@ class TopNav extends Component {
       <div className="top_nav">
         <a href="/" className="icon-arc-logo"></a>
         <div className="styles-top-menu">
-          <ul className={this.props.active == "our-brand" ? "active" : "inactive"}>
-            <a href = "/" style={{color: "white"}}> Brand Overview </a>
+          <ul
+            className={this.props.active == "our-brand" ? "active" : "inactive"}
+          >
+            <a href="/" style={{ color: "white" }}>
+              {" "}
+              Brand Overview{" "}
+            </a>
           </ul>
-          <ul className={this.props.active == "style-guide" ? "active" : "inactive"}>
-            <a href="/our-brand.html" style={{color: "white"}}>Style Guide</a>
-            </ul>
+          <ul
+            className={
+              this.props.active == "style-guide" ? "active" : "inactive"
+            }
+          >
+            <a href="/our-brand.html" style={{ color: "white" }}>
+              Style Guide
+            </a>
+          </ul>
         </div>
+        <ul className="top_nav_media_ul">
+          <a href="/" style={{ color: "white" }}>
+            {" "}
+            Media Kit
+          </a>
+        </ul>
       </div>
     );
   }
